9e4b79f52f1aadd4a40897b630e3ab2403a67a6c
Sergio Díaz <sergiodiazsifuentes@gmail.com>
2024-07-03 18:44:31 -0500

    Seeders Agregados

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -11,8 +11,12 @@ class DatabaseSeeder extends Seeder
     /**
      * Seed the application's database.
      */
-    public function run(): void
+    public function run()
     {
-        
+        $this->call([
+            NivelSeeder::class,
+            GradoSeeder::class,
+            SeccionSeeder::class,
+        ]);
     }
 }
