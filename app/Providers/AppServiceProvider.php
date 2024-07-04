4d617588dca61ff86e17f553263d8cf17009e0f8
Sergio Díaz <sergiodiazsifuentes@gmail.com>
2024-07-04 01:07:25 -0500

    CURSOS FINALIZADO, MERGE COMPLETO

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -4,7 +4,6 @@ namespace App\Providers;
 
 use Illuminate\Pagination\Paginator;
 use Illuminate\Support\ServiceProvider;
-use Illuminate\Pagination\Paginator;
 use Illuminate\Support\Facades\Schema;
 
 
