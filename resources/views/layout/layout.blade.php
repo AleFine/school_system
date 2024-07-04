25c42e8d52fee2d7c70b0bcfcdce051c69d512c0
Kevin <keving.kpg@gmail.com>
2024-07-04 02:50:02 -0500

    Final_4

--- resources/views/layout/layout.blade.php
+++ resources/views/layout/layout.blade.php
@@ -42,7 +42,7 @@
             <!-- Menu -->
             <aside id="layout-menu" class="layout-menu menu-vertical menu bg-menu-theme">
                 <div class="app-brand demo">
-                    <a href="index.html" class="app-brand-link">
+                    <a href="{{route('dashboard')}}" class="app-brand-link">
                         <span class="app-brand-logo demo">
                             <!-- SVG content -->
                         </span>
--- routes/web.php
+++ routes/web.php
@@ -28,7 +28,7 @@ Route::get('login', [AuthController::class, 'index'])->name('login');
 Route::post('post-login', [AuthController::class, 'postLogin'])->name('login.post');
 Route::get('registration', [AuthController::class, 'registration'])->name('register');
 Route::post('post-registration', [AuthController::class, 'postRegistration'])->name('register.post');
-Route::get('dashboard', [AuthController::class, 'dashboard']);
+Route::get('dashboard', [AuthController::class, 'dashboard'])->name('dashboard');
 Route::post('logout', [AuthController::class, 'logout'])->name('logout');
 
 
